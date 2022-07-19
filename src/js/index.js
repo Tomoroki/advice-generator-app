1a06e4dd59a56034b53106dc7e432a7ad71fbec6
Tomoroki <jorgechiva98@gmail.com>
2022-07-18 21:01:01 -0500

    Funcionamiento con ul>li

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,58 @@
 // Recuperar datos de la api y mostrarlos en .card__advice
 
 const API_URL = 'https://api.adviceslip.com/advice';
+const ul = document.querySelector(".ul");
+const dice = document.querySelector(".btn__dice");
 
-const HTMLResponse = document.querySelector(".card");
-const ul = document.createElement('ul');
-
-
-fetch(API_URL)
-    .then(response => response.json())
-    .then(users => {
-        // users.forEach(user => {
-        Object.keys(users).forEach(user => {
-            const slip = users[user];
-            let elem = document.createElement('li');
-            elem.appendChild(document.createTextNode(`${slip.id} - ${slip.advice}`));
-            ul.appendChild(elem);
-        });
-        HTMLResponse.appendChild(ul);
-    });
-
-const dice = document.querySelector(".card__dice");
-
-dice.addEventListener("click", _ => {
-    location.reload();
-});
+/* Forma 1 (se actualiza los datos RECARRGANDO la pagina y no por el DOM) */
+// const ul = document.createElement('ul');
 
 // fetch(API_URL)
-//         .then(userJson => userJson.json(), e => {
-//             console.log("Obtención fallida", e);
-//         })
-//         .then(userJson => {
-//             console.log(`Obtencion exitosa de ${userJson}`);
+//     .then(response => response.json())
+//     .then(users => {
+//         // users.forEach(user => {
+//         Object.keys(users).forEach(user => {
+//             const slip = users[user];
+//             let elem = document.createElement('li');
+//             elem.appendChild(document.createTextNode(`${slip.id} - ${slip.advice}`));
+//             ul.appendChild(elem);
+//         });
+//         HTMLResponse.appendChild(ul);
 //     });
-// Extract the data from the api and show in console
 
-// fetch(API_URL).then(response => response.json()).then(data => {
-//     console.log(data);
-// }).catch(error => {
-//     console.log(error);
-// }).finally(() => {
-//     console.log("Fin de la ejecución");
+// dice.addEventListener("click", _ => {
+//     location.reload();
 // });
+
+// Forma 2 (reacarga los datos por medio del DOM)
+getApi();
+function getApi() {
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(users => {
+            // users.forEach(user => {
+            Object.keys(users).forEach(user => {
+                const slip = users[user];
+                let elem = document.createElement('li');
+                elem.appendChild(document.createTextNode(`${slip.id} - ${slip.advice}`));
+                ul.appendChild(elem);
+            });
+        });
+};
+dice.addEventListener("click", () => {
+    // Si ya existe un elemento li en el html, lo eliminamos y se ejecuta la función
+    if (ul.querySelector("li")) {
+        ul.removeChild(ul.querySelector("li"));
+        // document.removeChild(card.querySelector("ul"));
+        getApi();
+    }
+    // Desabilitar el boton de tirar el dado por dos segundos despues de ejecutarse 
+    
+    // dice.classList.add("card__dice--disabled");
+    dice.disabled = true;
+    setTimeout(() => {
+        // dice.classList.remove("card__dice--disabled");
+        dice.disabled = false;
+    }, 2000);
+});
+
